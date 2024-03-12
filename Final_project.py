4f18ead76cdd3d9cd2c7ff753e8fc7f2cf593cd7
tayyaba97 <158370291+tayyaba97@users.noreply.github.com>
2024-03-11 22:12:10 -0600

    Add files via upload

---
+++ Final_project.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import yfinance as yf
+from bs4 import BeautifulSoup
+import numpy
+import requests 
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from datetime import datetime
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021--06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+
+#question 1- extract tesla stock using yfinance
+
+tesla = yf.Ticker('TSLA')
+tesla_data = tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+#print(tesla_data.head(5))
+
+#question 2- extract tesla revenue data using webscraping
+url = " https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
+html_data = requests.get(url).text
+soup = BeautifulSoup(html_data,"html5lib")
+tesla_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
+
+for table in soup.find_all('table'):
+
+    if ('Tesla Quarterly Revenue' in table.find('th').text):
+        rows = table.find_all('tr')
+        
+        for row in rows:
+            col = row.find_all('td')
+            
+            if col != []:
+                date = col[0].text
+                revenue = col[1].text.replace(',','').replace('$','')
+
+                tesla_revenue = tesla_revenue.append({"Date":date, "Revenue":revenue}, ignore_index=True)
+tesla_revenue["Revenue"] = tesla_revenue['Revenue'].str.replace(',|\$',"")
+tesla_revenue.dropna(inplace=True)
+
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+
+
+
+#question 3- use yfinance to extract stock data
+GameStop = yf.Ticker("GME")
+gme_data = GameStop.history(period="max")
+gme_data.reset_index(inplace=True)
+print(gme_data.head())
+
+#question 4 webscratping to extract stock data
+url = "https://www.macrotrends.net/stocks/charts/GME/gamestop/revenue"
+html_data = requests.get(url).text
+soup = BeautifulSoup(html_data, "html.parser")
+soup.find_all('title')
+gme_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
+
+for table in soup.find_all('table'):
+
+    if ('GameStop Quarterly Revenue' in table.find('th').text):
+        rows = table.find_all('tr')
+        
+        for row in rows:
+            col = row.find_all('td')
+            
+            if col != []:
+                date = col[0].text
+                revenue = col[1].text.replace(',','').replace('$','')
+
+                gme_revenue = gme_revenue.append({"Date":date, "Revenue":revenue}, ignore_index=True)
+gme_revenue.tail()
+
+# #question 5
+
+make_graph(tesla_data, tesla_revenue, 'Tesla')
+
+
+# #question 6
+
+make_graph(gme_data, gme_revenue, 'GameStop')
